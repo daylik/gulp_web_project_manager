c94458550f09b0501be2cd217f9e2f054e90502b
Олег Мешаев <dayll@yandex.ru>
2017-12-11 16:00:25 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
+//===== cmd ======
+//gulp
+//gulp --name project_name
+//buckup --name project_name
+//bild
+
+//gulp site --name site_name
+//gulp site --name max_pro
+
+
 // переменные ===================================
 var gulp                    = require('gulp'),
     replace                 = require('gulp-replace'),
@@ -7,9 +17,9 @@ var gulp                    = require('gulp'),
     zip                     = require('gulp-zip'),
     stylish                 = require('jshint-stylish'),
     uglify                  = require('gulp-uglify'),
-    //jshint 				= require('gulp-jshint'),
+    //jshint                = require('gulp-jshint'),
     filter                  = require('gulp-filter'),
-    //jade2php 				= require('gulp-jade-php'),
+    //jade2php              = require('gulp-jade-php'),
     cached                  = require('gulp-cached'),
     concat                  = require('gulp-concat'),
     postcss                 = require('gulp-postcss'),
@@ -17,12 +27,12 @@ var gulp                    = require('gulp'),
     sourcemaps              = require('gulp-sourcemaps'),
     sass                    = require('gulp-sass'),
     less                    = require('gulp-less'),
-    cssFormat 				= require('gulp-css-format'),
-    postcss_sass 			= require('postcss-sass'),
-    cssnano 			    = require('cssnano'),
+    cssFormat               = require('gulp-css-format'),
+    postcss_sass            = require('postcss-sass'),
+    cssnano                 = require('cssnano'),
     browserSync_localhost   = require('browser-sync').create(),
-    browserSync_proxy 	    = require('browser-sync').create(),
-    mainBowerFiles 		    = require('main-bower-files'),
+    browserSync_proxy       = require('browser-sync').create(),
+    mainBowerFiles          = require('main-bower-files'),
     imagemin                = require('gulp-imagemin'),
     replace_task            = require('gulp-replace-task'),
     args                    = require('yargs').argv,
@@ -30,7 +40,7 @@ var gulp                    = require('gulp'),
     node_path               = require('path'),
     newer                   = require('gulp-newer'),
     remember                = require('gulp-remember'),
-    autoprefixer    	    = require('gulp-autoprefixer');
+    autoprefixer            = require('gulp-autoprefixer');
 //=== GULP config file
 var GULP_config = require('./gulp_config.js');
 var SITES = GULP_config.config_ftp;
@@ -40,7 +50,7 @@ var ftp_params = GULP_config.ftp_set;
 var v = {};
     v.plugins_js = 'compiled_pack.js';
     v.plugins_css = 'plugins_compiled_pack.css';
-	v.scss_to_css = 'style_compiled.css';
+    v.scss_to_css = 'style_compiled.css';
     v.autoprefix_browsers_v1 = ['last 12 versions', '> 0.01%'];
     v.autoprefix_browsers_v2 = [
       "Android 2.3",
@@ -64,17 +74,18 @@ var local_project    = args.name || 'app';
 var site_name        = args.name || 'max_pro';
 
 var LOCAL = {
-    app: {
-        folder: 'app'
+    v1: {
+        folder: 'verstka_M'
     }
 };
 
+var LOCAL_project_PATH = LAB_local_folder + LOCAL[local_project].folder;
 //=== Задания ===================================
 
 gulp.task('task_browserSync_localhost', function() {
-    browserSync_localhost({
+    browserSync_localhost.init({
         server: {
-            baseDir: LOCAL[local_project].folder
+            baseDir: './'+ LOCAL_project_PATH +'/public'
         }
     })
 });
@@ -88,40 +99,54 @@ gulp.task('task_browserSyncProxy', function() {
 
 //=== FTP ================================
 gulp.task('ftp_send_all', function() {
-var FTP_connect = ftp.create( SITES[site_name].ftp );
+    var FTP_connect = ftp.create( SITES[site_name].ftp );
     gulp.src( LAB_FTP_folder + SITES[site_name].theme_folder + '/**')
         .pipe( FTP_connect.dest( SITES[site_name].ftp_path ));
 });
 
 gulp.task('ftp_send_css', function() {
 var FTP_connect = ftp.create( SITES[site_name].ftp );
-    gulp.src([ LAB_FTP_folder + SITES[site_name].theme_folder_css + '/*.{css,scss,sass,less}', LAB_FTP_folder + SITES[site_name].local_folder_css + '/*.{scss,sass,less}'])
+    gulp.src([ 
+        LAB_FTP_folder + SITES[site_name].theme_folder_css + '/*.{css,scss,sass,less}', 
+        LAB_FTP_folder + SITES[site_name].local_folder_css + '/*.{scss,sass,less}'
+        ])
         //.pipe( newer(SITES[site_name].theme_folder_css ))
         .pipe( FTP_connect.dest( SITES[site_name].ftp_path + SITES[site_name].ftp_folder.css ));
 });
 
 gulp.task('ftp_send_img', function() {
-var FTP_connect = ftp.create( SITES[site_name].ftp );
+    var FTP_connect = ftp.create( SITES[site_name].ftp );
     gulp.src( LAB_FTP_folder + SITES[site_name].theme_folder_img + '/*', { buffer: false })
         .pipe( newer( LAB_FTP_folder + SITES[site_name].theme_folder_img ))
         .pipe( FTP_connect.newer( SITES[site_name].ftp_path + SITES[site_name].ftp_folder.img ));
 });
 
+gulp.task('ftp_plugins_send_all', function() {
+    var FTP_connect = ftp.create( SITES[site_name].ftp );
+    gulp.src( LAB_FTP_folder + SITES[site_name].local_folder_plugins + '/**')
+        .pipe( FTP_connect.dest( SITES[site_name].ftp_plugins_path ));
+});
 
 //=== JADE ============================
 gulp.task('jade', function() {
-    return gulp.src( LAB_local_folder + LOCAL[local_project].folder + '/jade/**/*.jade')
+    return gulp.src( LOCAL_project_PATH + '/jade/**/*.jade')
         .pipe(jade({ pretty: true }))
-        .pipe(gulp.dest( LAB_local_folder + LOCAL[local_project].folder ))
+        .pipe(gulp.dest( LOCAL_project_PATH ))
+        .pipe(browserSync_localhost.reload({ stream: true }));
+});
+
+gulp.task('html', function() {
+    return gulp.src( LOCAL_project_PATH + '/source/**/*.html')
+        .pipe(gulp.dest( LOCAL_project_PATH +'/public' ))
         .pipe(browserSync_localhost.reload({ stream: true }));
 });
 
 gulp.task('jade_php', function() {
     return gulp.src( LAB_FTP_folder + SITES[site_name].theme_folder + '/**/*.jade')
-        //.pipe(jade({	locals: { title: 'jade_php' }, pretty:true }))
+        //.pipe(jade({  locals: { title: 'jade_php' }, pretty:true }))
         .pipe(jade({ pretty: true }))
         .pipe(rename({ extname: ".php" }))
-        .pipe(gulp.dest( SITES[site_name].theme_folder )); //.pipe(browserSync_localhost.reload({	stream: true }));
+        .pipe(gulp.dest( SITES[site_name].theme_folder )); //.pipe(browserSync_localhost.reload({   stream: true }));
 });
 
 //=== BOWER ===========================
@@ -133,15 +158,26 @@ gulp.task('vendor', function() {
         //.pipe(uglify())
         .pipe(concat(v.plugins_js))
         .pipe(gulp.dest( LOCAL[local_project].folder + '/js'));
-    //.pipe(browserSync_localhost.reload({	stream: true }));
+    //.pipe(browserSync_localhost.reload({  stream: true }));
 });
+
+gulp.task('js', function() {
+    return gulp.src( LOCAL_project_PATH + '/source/js/*.js')
+        //.pipe(jshint())
+        //.pipe(jshint.reporter(stylish))
+        //.pipe(uglify())
+    //.pipe(concat(v.plugins_js))
+    .pipe(gulp.dest( LOCAL_project_PATH + '/public/js'))
+    .pipe(browserSync_localhost.reload({ stream: true }));
+});
+
 gulp.task('vendor_css', function() {
     return gulp.src(mainBowerFiles())
         .pipe(filter('**/*.css'))
         //.pipe(cssnano())
         .pipe(concat(v.plugins_css))
         .pipe(gulp.dest( LOCAL[local_project].folder + '/css'));
-    //.pipe(browserSync_localhost.reload({	stream: true }));
+    //.pipe(browserSync_localhost.reload({  stream: true }));
 });
 
 // console_log functions
@@ -152,6 +188,24 @@ gulp.task('bower_files', function() {
 });
 
 //=== SCSS, SASS, POSTCSS > CSS ============
+gulp.task('local_scss', function() {
+    var processors = [
+        //autoprefixer( V_autoprefix_params ),
+        //postcss_sass(),
+        autoprefixer()
+        //, cssnano()
+    ];
+    return gulp.src( LOCAL_project_PATH + '/source/css/*.{sass,scss}')
+        .pipe(sass({
+            outputStyle: 'nested', //nested | compresed | compact
+            //includePaths: ['node_modules/susy/sass']
+        }).on('error', sass.logError))
+        //.pipe(postcss( processors ))
+        .pipe(rename(v.scss_to_css))
+        .pipe(gulp.dest( LOCAL_project_PATH + '/public/css'))
+        .pipe(browserSync_localhost.reload({ stream: true }));
+});
+
 gulp.task('local_css', function() {
     var processors = [
         //autoprefixer( V_autoprefix_params ),
@@ -159,14 +213,8 @@ gulp.task('local_css', function() {
         autoprefixer()
         //, cssnano()
     ];
-    return gulp.src( LAB_local_folder + LOCAL[local_project].folder + '/source/css/postcss/*.{sass,scss}')
-        .pipe(sass({
-            outputStyle: 'nested', //nested | compresed | compact
-            //includePaths: ['node_modules/susy/sass']
-        }).on('error', sass.logError))
-        //.pipe(postcss( processors ))
-        .pipe(rename(v.scss_to_css))
-        .pipe(gulp.dest( LAB_local_folder + LOCAL[local_project].folder + '/public/css'))
+    return gulp.src( LOCAL_project_PATH + '/source/css/*.css')
+        .pipe(gulp.dest( LOCAL_project_PATH + '/public/css'))
         .pipe(browserSync_localhost.reload({ stream: true }));
 });
 
@@ -269,20 +317,27 @@ gulp.task('bild', function(done) {
 });
 
 //=== CMD: gulp
-gulp.task('default', ['vendor', 'vendor_css', 'task_browserSync_localhost', 'local_css', 'jade'], function() {
-    gulp.watch( LAB_local_folder + LOCAL[local_project].folder + '/source/css/**/*.s*ss', ['local_css']);
-    gulp.watch( LAB_local_folder + LOCAL[local_project].folder + '/source/jade/**/*.jade', ['jade']);
-    gulp.watch( LAB_local_folder + LOCAL[local_project].folder + '/source/*.js', ['js']);
+gulp.task('default', [
+    'vendor', 
+    'vendor_css', 
+    'task_browserSync_localhost', 
+    'local_scss', 
+    'local_css', 
+    'jade', 
+    'html', 
+    'js'
+    ], function(){
+    
+    gulp.watch( LOCAL_project_PATH + '/source/css/**/*.css',  ['local_css']); 
+    gulp.watch( LOCAL_project_PATH + '/source/css/**/*.s*ss', ['local_scss']);
+    gulp.watch( LOCAL_project_PATH + '/source/jade/**/*.jade', ['jade']); 
+    gulp.watch( LOCAL_project_PATH + '/source/**/*.html',     ['html']);
+    gulp.watch( LOCAL_project_PATH + '/source/js/*.js',       ['js']);
 
-    //gulp.watch( LOCAL[local_project].folder + '/source/**/*.jade', ['jade']);
-    gulp.watch( LAB_local_folder + LOCAL[local_project].folder + '/source/css/*.s*ss', ['local_css']);
-    //gulp.watch([LOCAL[local_project].folder + '/public/css/*.css', LOCAL[local_project].folder + '/source/css/*.scss'], ['ftp_send']);
-    //gulp.watch(LAB_dir_1 + '/css/*.css', ['ftp_send']);
     //gulp.watch('app/*.html', browserSync.reload({stream: true}));
 });
 
 var reload_timer = 0;
-
 gulp.task('browser_reload', function(){
     reload_timer++;
     if( reload_timer == 1 ){
@@ -304,7 +359,12 @@ var browser_reload_func = function(){
 };
 
 //==== gulp site --name site_name
-gulp.task('site', ['remote_css', 'jade_php', 'imagemin', 'task_browserSyncProxy'], function() {
+gulp.task('site', [
+    'remote_css', 
+    'jade_php', 
+    'imagemin', 
+    'task_browserSyncProxy'
+    ], function() {
 
     gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder + '/**/*.jade', ['jade_php']);
     gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_css + '/*.s*ss', ['remote_css'])
@@ -318,6 +378,23 @@ gulp.task('site', ['remote_css', 'jade_php', 'imagemin', 'task_browserSyncProxy'
         //.on('change', browserSync_proxy.reload);
         //.on('change', function() { console.log('img changed!'); });
     gulp.watch( LAB_FTP_folder + SITES[site_name].theme_folder_img  + '/**', ['ftp_send_img']);
+});
+
+
+//==== gulp site_plugin --name site_name
+gulp.task('site_plugin', ['remote_plugins_css', 'task_browserSyncProxy'], function() {
+
+    //gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_plugins + '/**/*.jade', ['jade_php']);
+    gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_plugins + '/**/*.s*ss', ['remote_plugins_css']); //.on('unlink', function(filepath){ //remember.forget('remote_css', node_path.resolve(filepath));
+        //    delete chached.cashes.remote_css[ node_path.resolve(filepath)]; });
+    gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_plugins + '/**/*.**', ['ftp_plugins_send_all']);
+    gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_plugins + '/**/*.{css,scss,sass,js,php}', ['browser_reload']);
+    //.on('change', browserSync_proxy.reload);
+
+    //gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_plugins + '/**', ['imagemin']);
+        //.on('change', browserSync_proxy.reload);
+        //.on('change', function() { console.log('img changed!'); });
+    //gulp.watch( LAB_FTP_folder + SITES[site_name].local_folder_plugins  + '/**', ['ftp_send_img']);
 });
 
 
